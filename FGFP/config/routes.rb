702447d64ae9bc0a18d80525b6c5b3b74718c0d5
hmc-cs-lhernandezcruz <lhernandezcruz@cs.hmc.edu>
2018-02-24 15:47:56 -0800

    added paths to different pages. added footer and header

--- FGFP/config/routes.rb
+++ FGFP/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :profs
   resources :departments
+
+  root 'profs#index'
+  get '/index', to: 'profs#index'
+  get '/new', to: 'profs#new'
+  get '/show', to: 'profs#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
