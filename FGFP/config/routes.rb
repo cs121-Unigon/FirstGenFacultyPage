f917b96385bc9ea79abb9f9783c2aa31d110a079
D Agarwal <Dagarwal@Dakshas-MacBook-Pro.local>
2018-03-16 20:27:08 +0400

    added about page

--- FGFP/config/routes.rb
+++ FGFP/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/index', to: 'profs#index'
   get '/new', to: 'profs#new'
   get '/show', to: 'profs#show'
+  get '/about', to: 'profs#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "profs#index"
