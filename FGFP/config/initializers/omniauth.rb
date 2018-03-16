2b1796cc40958d7c8e1c4282d977ae77df5b2969
EleanorWhite <nwhite@g.hmc.edu>
2018-03-16 05:49:11 -0700

    Get sign in with Google

---
+++ FGFP/config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, '963732938905-17f87ptadejr5jmk073nka2d5dleurjs.apps.googleusercontent.com', '5pL_9AiG4F_FWhO60tBC5I7w', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+end
+
--- FGFP/db/schema.rb
+++ FGFP/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226004844) do
+ActiveRecord::Schema.define(version: 20180316123152) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20180226004844) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.index ["department_id"], name: "index_profs_on_department_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
