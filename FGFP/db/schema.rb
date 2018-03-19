f8ae940862814936e82ca7fec257eef6db10031d
hmc-cs-lhernandezcruz <lhernandezcruz@cs.hmc.edu>
2018-03-18 18:34:55 -0700

    simple feedback page. nonworking mailer

--- FGFP/db/schema.rb
+++ FGFP/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226004844) do
+ActiveRecord::Schema.define(version: 20180319012801) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
