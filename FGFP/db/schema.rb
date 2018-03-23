59a5825bb3a37eab51b630f151c78b81e55f283f
EleanorWhite <nwhite@g.hmc.edu>
2018-03-23 16:39:45 -0700

    Migrate database

--- FGFP/db/schema.rb
+++ FGFP/db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20180320234257) do
+
   create_table "departments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
