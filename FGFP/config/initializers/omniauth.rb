4a1838d82e4e271e96e1c294d084f8c98d8851c7
EleanorWhite <nwhite@g.hmc.edu>
2018-04-08 01:30:13 -0700

    try another csrf fix

--- FGFP/config/initializers/omniauth.rb
+++ FGFP/config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '963732938905-17f87ptadejr5jmk073nka2d5dleurjs.apps.googleusercontent.com', '5pL_9AiG4F_FWhO60tBC5I7w', { skip_jwt: true, access_type: 'offline', approval_prompt: 'auto', hd: 'g.hmc.edu' } 
+  provider :google_oauth2, '963732938905-17f87ptadejr5jmk073nka2d5dleurjs.apps.googleusercontent.com', '5pL_9AiG4F_FWhO60tBC5I7w', {:provider_ignores_state => true, skip_jwt: true, access_type: 'offline', approval_prompt: 'auto', hd: 'g.hmc.edu' } 
 end
 
