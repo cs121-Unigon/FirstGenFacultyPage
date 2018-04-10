ef5309b09aab4695ea92d3c3189882957f0eecf5
Chen Pekker <gpekker@hmc.edu>
2018-04-10 00:16:44 -0700

    fixed mainpage bugs

--- FGFP/db/schema.rb
+++ FGFP/db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180402051533) do
 
-
   create_table "departments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -30,7 +29,6 @@ ActiveRecord::Schema.define(version: 20180402051533) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type"
     t.string "option"
   end
 
