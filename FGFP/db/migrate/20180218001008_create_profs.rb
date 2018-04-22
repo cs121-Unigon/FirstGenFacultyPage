9613a0f7a47ed8f4729e2cbb32eb0236d8e5896d
Daksha Agarwal <dagarwal@g.hmc.edu>
2018-04-21 21:22:33 -0700

    undid changes to migration file

--- FGFP/db/migrate/20180218001008_create_profs.rb
+++ FGFP/db/migrate/20180218001008_create_profs.rb
@@ -2,15 +2,9 @@ class CreateProfs < ActiveRecord::Migration[5.1]
   def change
     create_table :profs do |t|
       t.string :name
-      t.string :lastname
-      t.string :pronouns
       t.string :email
       t.text :interests
-      t.text :lifestory
-      t.text :whymentor
-      t.text :funfact
       t.string :office
-      t.string :hometown
       t.belongs_to :department, foreign_key: true
 
       t.timestamps
