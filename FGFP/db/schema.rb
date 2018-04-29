bb58a5cfc65fe650bfce9fbc0ef695c07d56707a
chenphmc <31498869+chenphmc@users.noreply.github.com>
2018-04-28 18:29:37 -0700

    fixing merge conflicts

--- FGFP/db/schema.rb
+++ FGFP/db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402051533) do
+ActiveRecord::Schema.define(version: 20180422062856) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
@@ -45,6 +62,12 @@ ActiveRecord::Schema.define(version: 20180402051533) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer "user_id"
+    t.string "pronouns"
+    t.string "lastname"
+    t.string "hometown"
+    t.string "funfact"
+    t.string "lifestory"
+    t.string "whymentor"
     t.index ["department_id"], name: "index_profs_on_department_id"
     t.index ["user_id"], name: "index_profs_on_user_id"
   end
